b7abb38e568b368e4e3f0aa976ed4ce37a0b01ed
slimnation23 <slimnation2@gmail.com>
2020-03-22 14:26:37 +0200

    form1

--- contact.php
+++ contact.php
@@ -48,7 +48,7 @@ if(!$error)
 
 $name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
 
-$subject ="Новая заявка с сайта domain.name";
+$subject ="Новая заявка с сайта slimnation23.github.io/agency";
 $subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
 /*
 $message ="\n\nСообщение: ".$message."\n\nИмя: " .$name."\n\nТелефон: ".$tel."\n\n";
@@ -58,8 +58,8 @@ $message1 ="\n\nИмя: ".$name."\n\nТелефон: " .$tel."\n\nE-mail: " .$em
 
 $header = "Content-Type: text/plain; charset=utf-8\n";
 
-$header .= "From: Новая заявка <example@gmail.com>\n\n";	
-$mail = mail("example@gmail.com", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
+$header .= "From: Новая заявка <slimnation2@gmail.com>\n\n";	
+$mail = mail("slimnation2@gmail.com", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
 
 if($mail)
 {
