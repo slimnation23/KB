4a17f7d5fe0ac132a544d5add80993cb728ce9cc
slimnation23 <slimnation2@gmail.com>
2020-03-24 20:03:23 +0200

    activeTab

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let sectionImg1 = document.getElementById('sectionImg1');
 let sectionImg2 = document.getElementById('sectionImg2');
 let sectionImg3 = document.getElementById('sectionImg3');
 
-function selectTab1() {
+function selectTab1(a) {
     section1.style.display = 'block'
     sectionImg1.style.display = 'block'
     section2.style.display = 'none'
@@ -32,7 +32,20 @@ function selectTab3() {
     sectionImg3.style.display = 'block'
 }
 
+// Get the container element
+var btnContainer = document.getElementById("myDIV");
 
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("activeTab");
+    current[0].className = current[0].className.replace(" activeTab", "");
+    this.className += " activeTab";
+  });
+}
 
 
 
